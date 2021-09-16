324ab968bdba02f7734e9ff7d11fe167406b125f
Vlad Google <vladittrello@gmail.com>
2021-09-16 17:23:41 +0300

    HW_18 v.7.0

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,27 @@
-//import {UserDao} from "../db/user";
+import {UserDao} from "../db/user";
 import {UserService} from "../user/services";
-//import {User} from "../db/models/user";
+import {User} from "../db/models/user";
 //import {UserObj} from "../db/models/user"
-const userService = new UserService();
 
 
-const user = {
+
+const user = [{
+    name: "Vladislav",
+    fullName: "Googlya",
+    Age: 19,
+    email: "tutut@gmail.com",
+    password: "djdkjdk23"
+}] as User[];
+
+const user1 = {
+    name: "Vladislav",
+    fullName: "Googlya",
+    Age: 19,
+    email: "tutut@gmail.com",
+    password: "djdkjdk23"
+} as User;
+
+const user2 = {
     name: "Vladislav",
     fullName: "Googlya",
     Age: 19,
@@ -13,33 +29,110 @@ const user = {
     password: "djdkjdk23"
 };
 
+const id =1;
+const email = "vlad@gmail.com"
+
+
+
+
 const mockRegisterUser = jest.fn();
 jest.mock('../db/user', () => {
     return {
         UserDao: jest.fn().mockImplementation(() => {
-            return {registerUser: () => mockRegisterUser};
+            return {registerUser: mockRegisterUser};
         }),
     };
 })
+
+/*const mockGetUsers = jest.fn();
+jest.mock('../db/user', () => {
+    return {
+        UserDao: jest.fn().mockImplementation(() => {
+            return {getUsers: mockGetUsers};
+        }),
+    };
+})*/
 
 
 
 
 describe('Tests', () => {
+    let userService: UserService;
+
+    beforeAll(() => {
+        userService = new UserService();
+    })
 
 
-    it('create user should call function with params', async()=>{
-       await userService.regUser(user);
-        expect(mockRegisterUser()).toHaveBeenCalledWith(user);
+    afterEach(() => {
+        jest.clearAllMocks();
     });
 
-    /*it('create user should call function with params', async()=>{
-        const spy = jest.spyOn(UserService, "regUser").mockResolvedValue([]);
-        const users = await User.findAll();
-        expect(users).toEqual([]);
-        expect(spy).toHaveBeenCalledWith();
+
+    /*it('createUser should call function with params', async()=>{
+        await userService.regUser(user1);
+        expect(mockRegisterUser).toHaveBeenCalledWith(user1);
+    });
+
+    /*it('createUser should call function with params', async()=>{
+        await userService.getUsers();
+        expect(mockGetUsers).toHaveBeenCalledWith();
+    });*/
+
+
+        it('getUsers should call function with params', async()=>{
+            const spy = jest.spyOn(userService, "getUsers").mockResolvedValue(user);
+            const users = await userService.getUsers();
+            expect(users).toEqual(user);
+            expect(spy).toHaveBeenCalledWith();
+            expect(spy).toHaveBeenCalledTimes(1);
+            spy.mockRestore();
+        });
+
+    it('createUser should call function with params', async()=>{
+        const spy = jest.spyOn(userService, "regUser").mockResolvedValue(user1);
+        const users = await userService.regUser(user1);
+        expect(users).toEqual(user1);
+        expect(spy).toHaveBeenCalledWith(user1);
         expect(spy).toHaveBeenCalledTimes(1);
         spy.mockRestore();
-    });*/
+    });
+
+    it('updateUser should call function with params', async()=>{
+        const spy = jest.spyOn(userService, "updateUser").mockResolvedValue(user1);
+        const users = await userService.updateUser(id,user1);
+        expect(users).toEqual(user1);
+        expect(spy).toHaveBeenCalledWith(id,user1);
+        expect(spy).toHaveBeenCalledTimes(1);
+        spy.mockRestore();
+    });
+
+    it('deleteUser should call function with params', async()=>{
+        const spy = jest.spyOn(userService, "deleteUser").mockResolvedValue();
+        await userService.deleteUser(id);
+        expect(spy).toHaveBeenCalledWith(id);
+        expect(spy).toHaveBeenCalledTimes(1);
+        spy.mockRestore();
+    });
+
+    it('getUserById should call function with params', async()=>{
+        const spy = jest.spyOn(userService, "getUserById").mockResolvedValue(user1);
+        const users = await userService.getUserById(id);
+        expect(users).toEqual(user1);
+        expect(spy).toHaveBeenCalledWith(id);
+        expect(spy).toHaveBeenCalledTimes(1);
+        spy.mockRestore();
+    });
+
+    it('getUserByEmail should call function with params', async()=>{
+        const spy = jest.spyOn(userService, "getUserByEmail").mockResolvedValue(user1);
+        const users = await userService.getUserByEmail(email);
+        expect(users).toEqual(user1);
+        expect(spy).toHaveBeenCalledWith(email);
+        expect(spy).toHaveBeenCalledTimes(1);
+        spy.mockRestore();
+    });
+
+
 
 });
